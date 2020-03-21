e47a37e79393eb32632249c1247270f41a204a1e
Tristan Marsell <tristan@sqrls.de>
2020-03-21 17:58:55 +0100

    Fix data & add rate limiter

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/fiorix/go-redis v0.0.0-20160104010333-d987058b55eb
 	github.com/victorspringer/http-cache v0.0.0-20190721184638-fe78e97af707
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 )
--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"github.com/fiorix/go-redis/redis"
 	cache "github.com/victorspringer/http-cache"
 	"github.com/victorspringer/http-cache/adapter/memory"
+	"golang.org/x/time/rate"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -92,7 +96,7 @@ func getRedis(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 
-		if index == len(arr) {
+		if index != len(arr) {
 			finalString += key + ","
 		} else {
 			finalString += key
@@ -102,6 +106,74 @@ func getRedis(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte(finalString)); err != nil {
 		panic(err)
 	}
+}
+
+// Create a custom visitor struct which holds the rate limiter for each
+// visitor and the last time that the visitor was seen.
+type visitor struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+// Change the the map to hold values of the type visitor.
+var visitors = make(map[string]*visitor)
+var mu sync.Mutex
+
+// Run a background goroutine to remove old entries from the visitors map.
+func init() {
+	go cleanupVisitors()
+}
+
+func getVisitor(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	v, exists := visitors[ip]
+	if !exists {
+		limiter := rate.NewLimiter(1, 3)
+		// Include the current time when creating a new visitor.
+		visitors[ip] = &visitor{limiter, time.Now()}
+		return limiter
+	}
+
+	// Update the last seen time for the visitor.
+	v.lastSeen = time.Now()
+	return v.limiter
+}
+
+// Every minute check the map for visitors that haven't been seen for
+// more than 3 minutes and delete the entries.
+func cleanupVisitors() {
+	for {
+		time.Sleep(time.Minute)
+
+		mu.Lock()
+		for ip, v := range visitors {
+			if time.Since(v.lastSeen) > 3*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		limiter := getVisitor(ip)
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -127,7 +199,7 @@ func main() {
 		fmt.Println(err)
 	}
 
-	http.Handle("/outshine/client/send", http.HandlerFunc(sendRedis))
+	http.Handle("/outshine/client/send", limit(http.HandlerFunc(sendRedis)))
 	http.Handle("/outshine/client/get", http.HandlerFunc(getRedis))
 	http.Handle("/outshine/client/cached", cacheClient.Middleware(http.HandlerFunc(getRedis)))
 
